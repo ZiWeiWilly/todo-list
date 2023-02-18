cb3d379a8929cd1ea655aeaec337381287e6d57e
willy <m3iloveyoum3@gmail.com>
2023-02-18 16:04:35 +0800

    Complete First Version TodoList

---
+++ todo/src/TodoAddDialog.tsx
@@ -0,0 +1,151 @@
+import { FormEvent, useState } from 'react'
+import Button from '@mui/material/Button'
+import { Box } from '@mui/system';
+import TextField from '@mui/material/TextField'
+import Grid from '@mui/material/Grid'
+import MenuItem from '@mui/material/MenuItem'
+import { Todo } from './todo.types';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+
+const priorityOption = ['Low', 'Middle', 'High']
+
+type TodoAddDialogProps = {
+    refresh: () => void
+    close: () => void
+}
+
+export const TodoAddDialog = (props: TodoAddDialogProps) => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [due, setDue] = useState('');
+    const [place, setPlace] = useState('');
+    const [flag, setFlag] = useState('');
+    const [priority, setProprity] = useState<Todo['priority']>('Low');
+
+    const resetAddition = () => {
+        setTitle('')
+        setContent('')
+        setDue('')
+        setPlace('')
+        setFlag('')
+        setProprity('Low')
+    }
+
+    const handleSave = (createNext: boolean = false) => {
+        const body = {
+            title: title,
+            content: content,
+            due: due,
+            place: place,
+            flag: flag,
+            priority: priority,
+            isCompleted: false,
+        }
+
+        fetch('http://127.0.0.1:8000/api/todos/', {
+            method: 'POST',
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify(body),
+            redirect: 'follow'
+        })
+            .then(response => response.json())
+            .then(result => props.refresh())
+            .catch(error => console.log('error', error))
+        
+        if (createNext) {
+            resetAddition()
+        } else {
+            props.close()
+        }
+    }
+
+    return (
+        <Dialog open={true} onClose={props.close}>
+            <DialogTitle>新增待辦事項</DialogTitle>
+            <DialogContent>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            label="標題"
+                            size='small'
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)}
+                            required
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            label="內容"
+                            size='small'
+                            value={content}
+                            onChange={(e) => setContent(e.target.value)}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            label="截止日"
+                            size='small'
+                            type="date"
+                            value={due}
+                            onChange={(e) => setDue(e.target.value)}
+                            InputLabelProps={{ shrink: true }}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            label="地點"
+                            size='small'
+                            value={place}
+                            onChange={(e) => setPlace(e.target.value)}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            label="標籤"
+                            size='small'
+                            value={flag}
+                            onChange={(e) => setFlag(e.target.value)}
+                            fullWidth
+                        />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <TextField
+                            label="重要度"
+                            size='small'
+                            select
+                            value={priority}
+                            onChange={(e) => setProprity(e.target.value as Todo['priority'])}
+                            defaultValue="Low"
+                            fullWidth
+                        >
+                            {priorityOption.map((option) => (
+                                <MenuItem key={option} value={option}>{option}</MenuItem>
+                            ))}
+                        </TextField>
+                    </Grid>
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => handleSave(true)}>
+                    Save & Create Another
+                </Button>
+                <Button onClick={() => handleSave()}>
+                    Save
+                </Button>
+                <Button onClick={props.close}>
+                    Cancel
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
--- todo/src/TodoList.tsx
+++ todo/src/TodoList.tsx
@@ -1,8 +1,8 @@
-import React, { CSSProperties, useEffect, useState } from "react"
+import { CSSProperties, useEffect, useState } from "react"
 import Container from '@mui/material/Container';
 
 import { Todo } from "./todo.types"
-import { TodoAddition } from "./TodoAddition";
+import { TodoAddDialog } from "./TodoAddDialog";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -15,6 +15,11 @@ import ListIcon from '@mui/icons-material/List';
 import IconButton from '@mui/material/IconButton'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button'
+import RefreshIcon from '@mui/icons-material/Refresh';
+import { TodoEditDialog } from "./TodoEditDialog";
+import { TodoDeleteDialog } from "./TodoDeleteDialog";
+
 
 export const TodoList = () => {
     const styleAlignItems: CSSProperties = {
@@ -24,61 +29,139 @@ export const TodoList = () => {
     }
 
     const [todoItems, setTodoItems] = useState<Todo[]>([] as Todo[])
+    const [showButtons, setShowButtons] = useState<Boolean[]>([] as Boolean[])
+    const [showAddDialog, setShowAddDialog] = useState(false)
+    const [editItem, setEditItem] = useState<Todo | null>(null)
+    const [deleteItemId, setDeleteItemId] = useState<number | null>(null)
 
-    const handleCreateTodoItem = (item: Todo) => {
-        let curItems = todoItems.slice()
-        curItems.push(item) 
-        setTodoItems(curItems)
+    const handleFetchTodoItem = () => {
+        fetch('http://127.0.0.1:8000/api/todos/', {
+            method: 'GET',
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }),
+            redirect: 'follow'
+        })
+        .then(response => response.json())
+        .then((result) => {
+            setTodoItems(result)
+            setShowButtons(Array(result.length).fill(false))
+        })
+        .catch(error => console.log('error', error))
     }
 
-    const handleEditTodoItem = (item: Todo) => {
-        
+    const handleHover = (isHover: boolean, index: number) => {
+        let curShowButtons = showButtons.slice()
+        curShowButtons[index] = isHover
+        setShowButtons(curShowButtons)
     }
 
-    const handleDeleteTodoItem = (item: Todo) => {
-
-    }
+    useEffect(() => {
+        handleFetchTodoItem()
+    },[])
 
     return (
-        <Container maxWidth="md">
-            <TodoAddition onTodoItemAdd={handleCreateTodoItem}  />
-            <hr />
-            {todoItems.map((todo, index) => (
-                <Typography key={index} sx={{display: 'flex', alignItems: 'center'}}>
-                    <Accordion sx={{flex: '1', marginRight: '10px'}}>
-                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                            <Typography sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
-                                <div>{todo.title}</div>
-                                <div>{todo.due}</div>
-                            </Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Grid container spacing={2}>
-                                <Grid item xs={6}>
-                                    <div>{todo.content}</div>
-                                </Grid>
-                                <Grid item xs={6}>
-                                    <div style={styleAlignItems}>
-                                        <PlaceIcon sx={{marginRight: '10px'}} /> {todo.place}
-                                    </div>
-                                    <div style={styleAlignItems}>
-                                        <FlagIcon sx={{marginRight: '10px'}} /> {todo.flag}
-                                    </div>
-                                    <div style={styleAlignItems}>
-                                        <ListIcon sx={{marginRight: '10px'}} /> {todo.priority}
-                                    </div>
-                                </Grid>
-                            </Grid>
-                        </AccordionDetails>
-                    </Accordion>
-                    <IconButton aria-label="edit" onClick={() => handleEditTodoItem(todo)}>
-                        <EditIcon />
+        <>
+            <Container maxWidth="md">
+                <h3>
+                    待辦事項 
+                    <IconButton onClick={handleFetchTodoItem}>
+                        <RefreshIcon />
                     </IconButton>
-                    <IconButton aria-label="delete" onClick={() => handleDeleteTodoItem(todo)}>
-                        <DeleteIcon />
-                    </IconButton>
-                </Typography>
-            ))}
-        </Container>
+                </h3>
+                {todoItems.map((todo, index) => (
+                    <div 
+                        key={index} 
+                        onMouseEnter={() => handleHover(true, index)} 
+                        onMouseLeave={() => handleHover(false, index)}
+                    >
+                        <Typography sx={{display: 'flex', alignItems: 'center'}}>
+                            <Accordion sx={{flex: '1', marginRight: '10px', marginBottom: '1px'}}>
+                                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography sx={{display: 'flex', justifyContent: 'space-between', width: '100%'}}>
+                                        <div>{todo.title}</div>
+                                        <div>{todo.due}</div>
+                                    </Typography>
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={6}>
+                                            <div>{todo.content}</div>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <div style={styleAlignItems}>
+                                                <PlaceIcon sx={{marginRight: '10px'}} /> {todo.place}
+                                            </div>
+                                            <div style={styleAlignItems}>
+                                                <FlagIcon sx={{marginRight: '10px'}} /> {todo.flag}
+                                            </div>
+                                            <div style={styleAlignItems}>
+                                                <ListIcon sx={{marginRight: '10px'}} /> {todo.priority}
+                                            </div>
+                                        </Grid>
+                                    </Grid>
+                                </AccordionDetails>
+                            </Accordion>
+                            {showButtons[index]
+                            ? 
+                                <>
+                                    <IconButton aria-label="edit" onClick={() => setEditItem(todo)}>
+                                        <EditIcon />
+                                    </IconButton>
+                                    <IconButton aria-label="delete" onClick={() => setDeleteItemId(todo.id)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </>
+                            :
+                                <></>
+                            }
+                        </Typography>
+                    </div>
+                ))}
+                <Button 
+                    variant="outlined" 
+                    onClick={() => setShowAddDialog(true)}
+                    sx={{marginTop: '10px'}}
+                >
+                    新增
+                </Button>
+            </Container>
+            {showAddDialog
+                ?
+                    <>
+                        <TodoAddDialog 
+                            close={() => setShowAddDialog(false)} 
+                            refresh={handleFetchTodoItem}
+                        />
+                    </>
+                :
+                    <></>
+            }
+            {editItem
+                ?
+                    <>
+                        <TodoEditDialog 
+                            editTodoItem={editItem} 
+                            close={() => setEditItem(null)}  
+                            refresh={handleFetchTodoItem}
+                        />
+                    </>
+                :
+                    <></>
+            }
+            {deleteItemId
+                ?
+                    <>
+                        <TodoDeleteDialog 
+                            itemId={deleteItemId} 
+                            close={() => setDeleteItemId(null)} 
+                            refresh={handleFetchTodoItem}
+                        />
+                    </>
+                :
+                    <></>
+            }
+        </>
     )
 }
